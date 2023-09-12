8039405a5680f307c4fd78acb2a25eb48c7c7361
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2023-09-12 20:31:59 +0300

    added

---
+++ Inhetirance/inheritanceDemo/src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        OgretmenKrediManager ogretmenKrediManager =new OgretmenKrediManager();
+        ogretmenKrediManager.Hesapla();
+        KrediUI krediUI = new KrediUI();
+        krediUI.KrediHesapla(new OgretmenKrediManager());
+        krediUI.KrediHesapla(new TarimKrediManager());
+
+    }
+}
