efb8cc1c238182b1624178085aca23a4e340e656
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2023-09-12 20:40:15 +0300

    added

---
+++ Abstract/abstractDemo/src/OracleDatabaseManager.java
@@ -0,0 +1,6 @@
+public class OracleDatabaseManager extends BaseDatabaseManager{
+    @Override
+    public void getdata() {
+        System.out.println("Veri Getirildi: Oracle");
+    }
+}
---
+++ Abstract/abstractDemo/src/SqlServerDatabaseManager.java
@@ -0,0 +1,6 @@
+public class SqlServerDatabaseManager extends BaseDatabaseManager{
+    @Override
+    public void getdata() {
+        System.out.println("Veri Getirildi: SQL Server");
+    }
+}
